08115c6cc071489b87613de59c0e64b8d068cb95
muhammedshamilmt <muhammedhishamptb@gmail.com>
2025-06-08 23:12:31 +0530

    alatics updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { UserProvider } from '@/contexts/user-context'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -40,6 +41,7 @@ export default function RootLayout({
             disableTransitionOnChange={false}
           >
             {children}
+            <Analytics />
             <Toaster />
           </ThemeProvider>
         </UserProvider>
